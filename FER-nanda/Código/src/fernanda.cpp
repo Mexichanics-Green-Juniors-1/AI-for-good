1aae4f8522a4555e93819774f8bccecebbc2880a
Axxel-otl <Axxel.Inkordia@proton.me>
2026-04-15 23:23:44 -0600

    Emm... Si (#36)

---
+++ FER-nanda/Código/src/fernanda.cpp
@@ -0,0 +1,122 @@
+/*
+    Autor: Mexichanics Green (Jorge)
+    Programa: FER-nanda
+*/
+
+// Librerías
+#include<Arduino.h>
+#include<Servo.h>
+
+// Enums
+enum State{
+    Stop,
+    Forward,
+    Backward,
+    Left,
+    Right,
+    Sensing,
+    ServoChanging
+}, ServoHeight{
+    Up,
+    Down
+};
+// Variables
+State btState = Stop;
+ServoHeight ServoState = Up;
+HardwareSerial &Bluetooth = Serial1;
+int minHeight = 30, maxHeight = 0;
+// Pines
+uint8_t IN[] = {8, 9, 10, 11};
+uint8_t motor = 12, yl = A0;
+// Servos
+Servo Motor;
+
+// Actualizar State usando BT
+State btUpdate(HardwareSerial &btHere){
+    char dato = btHere.read();
+    switch(dato) {
+        case 'S': return(Stop);    break;
+        case 'F': return(Forward); break;
+        case 'B': return(Backward);break;
+        case 'L': return(Left);    break;
+        case 'R': return(Right);   break;
+        case 'V': return(ServoChanging); break;
+        case 'X': return(Sensing); break;
+        default: return(Stop); break;
+    }
+}
+
+void setup(){
+    Serial.begin(9600);
+    Bluetooth.begin(9600);
+    for(uint8_t i = 0; i < 4; i++){
+        pinMode(IN[i], OUTPUT);
+    }
+    Motor.attach(motor);
+    Motor.write(0);
+}
+
+void loop(){
+    if(Bluetooth.available()){
+        btState = btUpdate(Bluetooth);
+    }
+    if(ServoState == Up){ // Así ni el servo ni el sensor se dañan porque el robot avanza mientras están ente
+        switch(btState){
+            case Stop:
+                digitalWrite(IN[0], LOW);
+                digitalWrite(IN[1], LOW);
+                digitalWrite(IN[2], LOW);
+                digitalWrite(IN[3], LOW);
+            break;
+            case Forward:
+                digitalWrite(IN[0], HIGH);
+                digitalWrite(IN[1], LOW);
+                digitalWrite(IN[2], HIGH);
+                digitalWrite(IN[3], LOW);
+            break;
+            case Backward:
+                digitalWrite(IN[0], LOW);
+                digitalWrite(IN[1], HIGH);
+                digitalWrite(IN[2], LOW);
+                digitalWrite(IN[3], HIGH);
+            break;
+            case Left:
+                digitalWrite(IN[0], LOW);
+                digitalWrite(IN[1], HIGH);
+                digitalWrite(IN[2], HIGH);
+                digitalWrite(IN[3], LOW);
+            break; 
+            case Right:
+                digitalWrite(IN[0], HIGH);
+                digitalWrite(IN[1], LOW);
+                digitalWrite(IN[2], LOW);
+                digitalWrite(IN[3], HIGH);
+            break;
+            default:
+                Serial.println("Error");
+            break;
+        }
+    } else if(btState == Sensing){ // Entonces, si el Servo NO está arriba (Osea, está abajo), va a revisar si está sensando, y en caso de estarlo, sensar
+        int lectura = analogRead(yl);
+        int humedad = map(lectura, 1023, 0, 0, 100); 
+        Bluetooth.print("Humedad detectada: ");
+        Bluetooth.print(humedad);
+        Bluetooth.println("%");
+        btState = Stop;
+    }
+    if(btState == ServoChanging && ServoState == Up){ // Si el servo está arriba y está cambiando baja, y si está abajo sube
+        Motor.write(minHeight);
+        ServoState = Down;
+        btState = Stop;
+    } else if(btState == ServoChanging && ServoState == Down){
+       Motor.write(maxHeight);
+       ServoState = Up;
+       btState = Stop;
+    }
+    if(ServoState == Down){
+        digitalWrite(IN[0], LOW);
+        digitalWrite(IN[1], LOW);
+        digitalWrite(IN[2], LOW);
+        digitalWrite(IN[3], LOW);
+    }
+}
--- FER-nanda/Idea del proyecto.md
+++ FER-nanda/Idea del proyecto.md
@@ -1,10 +1,16 @@
 ## FER-nanda
 Nuestro proyecto se llama FER-nanda (Formación de Elección Robótica). Se llama así porque consiste en un robot con múltiples sensores diseñado para que los agricultores puedan definir puntos importantes en un lugar o área que planean utilizar, por ejemplo: Medir el nivel del suelo; Medir el nivel de humedad; Medir la temperatura; entre otros posibles riesgos para el cultivo.
+
 ### El origen de la idea
 Nuestro equipo se propuso diseñar un proyecto que ayude realmente a los agricultores de todo el mundo, por lo que nos planteamos ¿Qué podría ayudar sin causar un efecto negativo, y que realmente ayude a reducir los problemas de los agricultores? Tuvimos una lluvia de ideas colaborativa hasta que alguien dio la idea: ¿Qué tal si hacemos un robot para escoger y valorar terrenos?
+
 ### A futuro
 La idea es que en un tiempo tengamos un robot completo, que sea teleoperado para evitar que los agricultores se asoleen demasiado por culpa de tener que revisar las condiciones del entorno, alimentado por energía limpia como paneles solares, o energía eólica almacenada que luego cargue al robot. También tendrá más sensores y usaremos una cámara junto a modelos básicos de IA para detectar plagas y alejarlas sin hacer daño a las plagas, al ambiente o a las plantas.
+
+### Comportamiento
+![img](./Diagrama%20FER.png)
 
 ### Índice
 - [Modelo 3D](./FER-nanda.stl)
 - [Materiales](./Materiales%20(FER-nanda).md)
+- [Link](https://cad.onshape.com/documents/8b8a834fbd4b85bb0eff8fca/w/bd1cbc69c4f7d92c1fcc2a74/e/b71a2af07e83075286ce13ed)
