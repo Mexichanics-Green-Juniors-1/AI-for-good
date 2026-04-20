30635e456f4de5fe09f37cc36dff6dfcd24d4b5e
Axxel-otl <Axxel.Inkordia@proton.me>
2026-04-20 08:01:28 -0600

    .

--- FER-nanda/Código/src/fernanda.cpp
+++ FER-nanda/Código/src/fernanda.cpp
@@ -3,10 +3,12 @@
     Programa: FER-nanda
 */
 
+// Librerías
 #include <Arduino.h>
 #include <Servo.h>
 
-enum State {
+// Enums
+enum State{
     Stop,
     Forward,
     Backward,
@@ -15,138 +17,108 @@ enum State {
     Sensing,
     ServoChanging
 };
-
-enum ServoHeight {
+enum ServoHeight{
     Up,
     Down
 };
 
-State btState = Stop;
-ServoHeight ServoState = Up;
-
+// Bluetooth
 HardwareSerial &Bluetooth = Serial1;
+State btState = Stop;
 
-const int minHeight = 30;
-const int maxHeight = 0;
+// Servo
+ServoHeight ServoState = Up;
+Servo Motor;
 
-uint8_t IN[] = {8, 9, 10, 11};
+// Pines
+uint8_t IN[] = {8,9,10,11};
 uint8_t motorPin = 12;
 uint8_t yl = A0;
 
-Servo Motor;
+// Variables
+const int minHeight = 30;
+const int maxHeight = 0;
+unsigned long lastServoChange = 0;
+const int servoDelay = 300;
 
-State btUpdate(HardwareSerial &btHere) {
-    if (!btHere.available()) return Stop;
-
+// Función BT
+State btUpdate(HardwareSerial &btHere){
+    if(!btHere.available()) return(btState);
     char dato = btHere.read();
-
-    switch (dato) {
-        case 'S': return Stop;
-        case 'F': return Forward;
-        case 'B': return Backward;
-        case 'L': return Left;
-        case 'R': return Right;
-        case 'V': return ServoChanging;
-        case 'X': return Sensing;
-        default: return Stop;
+    switch(dato){
+        case 'S': return(Stop);
+        case 'F': return(Forward);
+        case 'B': return(Backward);
+        case 'L': return(Left);
+        case 'R': return(Right);
+        case 'V': return(ServoChanging);
+        case 'X': return(Sensing);
+        default: return(btState);
     }
 }
 
-void setup() {
+void setup(){
     Serial.begin(9600);
     Bluetooth.begin(9600);
-
-    for (uint8_t i = 0; i < 4; i++) {
+    for(uint8_t i = 0; i < 4; i++){
         pinMode(IN[i], OUTPUT);
     }
-
     Motor.attach(motorPin);
     Motor.write(maxHeight);
 }
 
-void loop() {
-
+void loop(){
     btState = btUpdate(Bluetooth);
-
-    // ---------------- MOVIMIENTO ----------------
-    if (ServoState == Up) {
-
-        if (btState == Stop) {
-            digitalWrite(IN[0], LOW);
-            digitalWrite(IN[1], LOW);
-            digitalWrite(IN[2], LOW);
-            digitalWrite(IN[3], LOW);
-        }
-
-        else if (btState == Forward) {
-            digitalWrite(IN[0], HIGH);
-            digitalWrite(IN[1], LOW);
-            digitalWrite(IN[2], HIGH);
-            digitalWrite(IN[3], LOW);
-        }
-
-        else if (btState == Backward) {
-            digitalWrite(IN[0], LOW);
-            digitalWrite(IN[1], HIGH);
-            digitalWrite(IN[2], LOW);
-            digitalWrite(IN[3], HIGH);
-        }
-
-        else if (btState == Left) {
-            digitalWrite(IN[0], LOW);
-            digitalWrite(IN[1], HIGH);
-            digitalWrite(IN[2], HIGH);
-            digitalWrite(IN[3], LOW);
-        }
-
-        else if (btState == Right) {
-            digitalWrite(IN[0], HIGH);
-            digitalWrite(IN[1], LOW);
-            digitalWrite(IN[2], LOW);
-            digitalWrite(IN[3], HIGH);
-        }
-
-        else {
-            digitalWrite(IN[0], LOW);
-            digitalWrite(IN[1], LOW);
-            digitalWrite(IN[2], LOW);
-            digitalWrite(IN[3], LOW);
-        }
-    }
-
-    // ---------------- SENSOR ----------------
-    else if (btState == Sensing && ServoState == Down) {
-
-        int lectura = analogRead(yl);
-        int humedad = map(lectura, 1023, 0, 0, 100);
-
-        Bluetooth.print("Humedad: ");
-        Bluetooth.print(humedad);
-        Bluetooth.println("%");
-
-        btState = Stop;
-    }
-
-    // ---------------- SERVO ----------------
-    if (btState == ServoChanging) {
-
-        if (ServoState == Up) {
+    if(btState == ServoChanging && millis() - lastServoChange > servoDelay){
+        if(ServoState == Up){
             Motor.write(minHeight);
             ServoState = Down;
-        }
-        else {
+        } else{
             Motor.write(maxHeight);
             ServoState = Up;
         }
-
+        lastServoChange = millis();
         btState = Stop;
     }
-
-    // seguridad motor apagado si está abajo
-    if (ServoState == Down) {
+    if(ServoState == Down){
         digitalWrite(IN[0], LOW);
         digitalWrite(IN[1], LOW);
         digitalWrite(IN[2], LOW);
         digitalWrite(IN[3], LOW);
+    } else{
+        if(btState == Stop){
+            digitalWrite(IN[0], LOW);
+            digitalWrite(IN[1], LOW);
+            digitalWrite(IN[2], LOW);
+            digitalWrite(IN[3], LOW);
+        } else if(btState == Forward){
+            digitalWrite(IN[0], HIGH);
+            digitalWrite(IN[1], LOW);
+            digitalWrite(IN[2], HIGH);
+            digitalWrite(IN[3], LOW);
+        } else if(btState == Backward){
+            digitalWrite(IN[0], LOW);
+            digitalWrite(IN[1], HIGH);
+            digitalWrite(IN[2], LOW);
+            digitalWrite(IN[3], HIGH);
+        } else if(btState == Left){
+            digitalWrite(IN[0], LOW);
+            digitalWrite(IN[1], HIGH);
+            digitalWrite(IN[2], HIGH);
+            digitalWrite(IN[3], LOW);
+        } else if(btState == Right){
+            digitalWrite(IN[0], HIGH);
+            digitalWrite(IN[1], LOW);
+            digitalWrite(IN[2], LOW);
+            digitalWrite(IN[3], HIGH);
+        }
+    }
+    if(btState == Sensing && ServoState == Down){
+        int lectura = analogRead(yl);
+        int humedad = map(lectura, 1023, 0, 0, 100);
+        Bluetooth.print("Humedad:");
+        Bluetooth.print(humedad);
+        Bluetooth.println("%");
+        btState = Stop;
     }
 }
--- FER-nanda/Código/src/rc.cpp
+++ FER-nanda/Código/src/rc.cpp
@@ -3,86 +3,95 @@
     Programa: FER-nanda RC
 */
 
+// Librerías
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 #include <LiquidCrystal_I2C.h>
 
+// Bluetooth
 BluetoothSerial BT;
+uint8_t address[] = {0x00,0x23,0x11,0x01,0x23,0xB0};
+char lastSent = 'S';
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+// Pantalla
+LiquidCrystal_I2C lcd(0x27,16,2);
 
+// Pines
 const uint8_t joyX = 34;
 const uint8_t joyY = 35;
 const uint8_t joyButton = 32;
-
 const uint8_t button = 33;
 
+// Umbrales
 const int thresholdLow = 300;
 const int thresholdHigh = 700;
-const int deadZoneMin = 400;
-const int deadZoneMax = 600;
 
-char lastSent = 'S';
-
+// Variables
 unsigned long lastBtn1 = 0;
 unsigned long lastBtn2 = 0;
 const int debounceDelay = 250;
 
-void setup() {
+void setup(){
     Serial.begin(115200);
-
-    BT.begin("FER-nanda_RC", true);
-
+    if(!BT.begin("FER-nanda_RC",true)){
+        Serial.println("Error BT");
+        return;
+    }
+    Serial.println("Conectando a HC-06...");
+    if(BT.connect(address)){
+        Serial.println("Conectado");
+    } else{
+        Serial.println("No conectado");
+    }
     pinMode(joyButton, INPUT_PULLUP);
     pinMode(button, INPUT_PULLUP);
-
     lcd.init();
     lcd.backlight();
-
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0,0);
     lcd.print("RC Ready");
 }
 
-void loop() {
-
+void loop(){
+    if(!BT.connected()){
+        Serial.println("Reconectando...");
+        if(BT.connect(address)){
+            Serial.println("Reconectado");
+        } else{
+            Serial.println("Fallo reconexion");
+        }
+        delay(2000);
+        return;
+    }
     char command = 'S';
-
-    if (!digitalRead(button) && millis() - lastBtn1 > debounceDelay) {
+    if(!digitalRead(button) && millis() - lastBtn1 > debounceDelay){
         command = 'X';
         lastBtn1 = millis();
-    }
-    else if (!digitalRead(joyButton) && millis() - lastBtn2 > debounceDelay) {
+    } else if(!digitalRead(joyButton) && millis() - lastBtn2 > debounceDelay){
         command = 'V';
         lastBtn2 = millis();
-    }
-    else {
+    } else{
         int x = analogRead(joyX);
         int y = analogRead(joyY);
-
-        if (y > thresholdHigh) command = 'F';
-        else if (y < thresholdLow) command = 'B';
-        else if (x > thresholdHigh) command = 'R';
-        else if (x < thresholdLow) command = 'L';
+        if(y > thresholdHigh) command = 'F';
+        else if(y < thresholdLow) command = 'B';
+        else if(x > thresholdHigh) command = 'R';
+        else if(x < thresholdLow) command = 'L';
         else command = 'S';
     }
-
-    if (command != lastSent) {
+    if(command != lastSent){
         BT.write(command);
         lastSent = command;
     }
-
-    String msg = "";
-
-    while (BT.available()) {
-        msg += (char)BT.read();
+    char msg[33];
+    int i = 0;
+    while(BT.available() && i < 32){
+        msg[i++] = BT.read();
     }
-
-    if (msg.length() > 0) {
-        lcd.setCursor(0, 1);
-        lcd.print("                ");
-        lcd.setCursor(0, 1);
+    msg[i] = '\0';
+    if(i > 0){
+        lcd.setCursor(0,1);
         lcd.print(msg);
+        lcd.print("                ");
     }
-
     delay(40);
 }
